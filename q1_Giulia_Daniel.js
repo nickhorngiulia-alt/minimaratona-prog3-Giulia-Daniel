b9096d6538a5dc76f06e44939dff0b18ee30f0f6
nickhorngiulia-alt <nickhorngiulia@gmail.com>
2026-04-14 15:44:10 -0300

    Q1

---
+++ q1_Giulia_Daniel.js
@@ -0,0 +1,28 @@
+
+let numeros = [];
+function q1(){
+    let n = parseInt(document.querySelector("#campo").value);
+for (let i = 1; i <= n; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        m5e3 = "FizzBuzz"
+        numeros.push(m5e3);
+    } else if (i % 3 == 0){
+        let m3 = "Fizz"
+        numeros.push(m3);
+     } else if (i % 5 == 0) {
+        let m5 = "Buzz"
+        numeros.push(m5);
+    } else if (i % 7 == 0){
+        m7 = "boom";
+        numeros.push(m7);
+    } else {
+        numeros.push(i);
+    };
+    alert(i)
+}
+alert(numeros);
+numeros = [];
+}
+
+//document.querySelector("#saida").textContent = numeros;
+document.querySelector("#btnExecutar").addEventListener("click",q1);
